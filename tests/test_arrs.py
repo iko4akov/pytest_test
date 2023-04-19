6afb23bab19f9cb066db80e4a2464728fe278c5a
iko4akov <iko4akov@gmail.com>
2023-04-19 20:01:32 +0300

    change test_arrs.py

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,9 +1,14 @@
+import pytest
+
 from utils import arrs
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([1, 2, 3], 1, "test") == 2
+
+def test_get_none_list():
+    with pytest.raises(IndexError):
+        arrs.get([], 0, "test")
 
 
 def test_slice():
